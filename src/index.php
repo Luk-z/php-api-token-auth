68db8a5fa3993f8116e5463aaa35ac57cccf5e19
Luca Ziliani <lucagcc@gmail.com>
2022-12-09 00:50:34 +0100

    :white_check_mark: add phpunit

--- src/index.php
+++ src/index.php
@@ -5,15 +5,15 @@
  * - try to use app('db') from Lumen
 */
 
-define("PATA_DIR", __DIR__);
+// define("PATA_DIR", __DIR__);
 
-require_once PATA_DIR.'/constants.php';
-require_once PATA_DB_PATH.'/DbInterface.php';
-require_once PATA_SECURITY_PATH.'/Hash.php';
-require_once PATA_DIR.'/PATA.php';
-require_once PATA_HELPERS_PATH.'/DbHelper.php';
-require_once PATA_HELPERS_PATH.'/AppHelper.php';
-require_once PATA_HELPERS_PATH.'/DateTimeHelper.php';
-require_once PATA_HELPERS_PATH.'/ValidateHelper.php';
-require_once PATA_HELPERS_PATH.'/AuthHelper.php';
-require_once PATA_HELPERS_PATH.'/HashHelper.php';
+// require_once PATA_DIR.'/constants.php';
+// require_once PATA_DB_PATH.'/DbInterface.php';
+// require_once PATA_SECURITY_PATH.'/Hash.php';
+// require_once PATA_DIR.'/PATA.php';
+// require_once PATA_HELPERS_PATH.'/DbHelper.php';
+// require_once PATA_HELPERS_PATH.'/AppHelper.php';
+// require_once PATA_HELPERS_PATH.'/DateTimeHelper.php';
+// require_once PATA_HELPERS_PATH.'/ValidateHelper.php';
+// require_once PATA_HELPERS_PATH.'/AuthHelper.php';
+// require_once PATA_HELPERS_PATH.'/HashHelper.php';
---
+++ tests/DemoTest.php
@@ -0,0 +1,9 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class DemoTest extends TestCase{
+    public function testDemo(){
+        $this->assertEquals(true, true);
+    }
+}
