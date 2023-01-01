bf61dfcecda89e4156035dcccbed099257458d7f
Luca Ziliani <lucagcc@gmail.com>
2023-01-01 13:27:22 +0100

    :bug: import LumenDB and LumenHash in index.php

--- src/index.php
+++ src/index.php
@@ -6,7 +6,9 @@ define('PATA_DIR', __DIR__);
 
 require_once PATA_DIR . '/constants.php';
 require_once PATA_DB_PATH . '/DbInterface.php';
+require_once PATA_DB_PATH . '/LumenDB.php';
 require_once PATA_SECURITY_PATH . '/Hash.php';
+require_once PATA_SECURITY_PATH . '/LumenHash.php';
 require_once PATA_DIR . '/PATA.php';
 require_once PATA_HELPERS_PATH . '/DbHelper.php';
 require_once PATA_HELPERS_PATH . '/AppHelper.php';
